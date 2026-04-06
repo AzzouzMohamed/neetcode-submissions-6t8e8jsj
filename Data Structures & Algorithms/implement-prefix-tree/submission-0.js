618dfe1addbe74d6ca7d4527644100e908efeb58
AzzouzMohamed <67800539+AzzouzMohamed@users.noreply.github.com>
2026-04-06 14:34:23 +0200

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.js
@@ -0,0 +1,72 @@
+class TreeNode {
+    constructor(isThisTheEnd = false){
+       this.children = new Map();
+       this.isThisTheEnd = isThisTheEnd;
+    }
+}
+
+
+
+class PrefixTree {
+    constructor() {
+        this.root = new TreeNode();
+    }
+
+    /**
+     * @param {string} word
+     * @return {void}
+     */
+     insert(word) {
+        let pointer = this.root;
+        for(let i = 0 ; i < word.length ; i++){
+            if(!pointer.children.has(word[i])){
+                pointer.children.set(word[i], new TreeNode(i === word.length - 1));
+            } else {
+                if(i === word.length - 1 ){
+                    let char = pointer.children.get(word[i]);
+                    char.isThisTheEnd = true;
+                }
+
+            }
+            pointer = pointer.children.get(word[i]);
+        }
+
+        return;
+    }
+
+
+    /**
+     * @param {string} word
+     * @return {boolean}
+     */
+    search(word) {
+        let pointer = this.root;
+
+        for(let i = 0 ; i < word.length ; i++){
+            if(!pointer.children.has(word[i])){
+                return false;
+            }
+            pointer = pointer.children.get(word[i]);
+        }
+
+        return pointer.isThisTheEnd;    
+     
+    }
+
+    /**
+     * @param {string} prefix
+     * @return {boolean}
+     */
+    startsWith(prefix) {
+        let pointer = this.root;
+
+       for(let i = 0 ; i < prefix.length ; i++){
+            if(!pointer.children.has(prefix[i])){
+                return false;
+            }
+            pointer = pointer.children.get(prefix[i]);
+        }
+
+        return true; 
+    }
+}
